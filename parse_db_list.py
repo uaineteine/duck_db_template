02764db1c74bfe7d9ee25a7255ac956c2c911e7a
Daniel Stamer-Squair <uaine.teine@hotmail.com>
2025-02-01 21:28:01 +1100

    another verification check on the db list

--- parse_db_list.py
+++ parse_db_list.py
@@ -12,17 +12,30 @@ def clean_db_list(df):
         "Frame was cleaned to remove whitespace, please fix this in your list"
     return df_cleaned
 
+def verify_if_any_duplicates(df):
+    all_names = df["DB_NAME"]
+    if all_names.duplicated().any():
+        raise ValueError("DB_LIST has duplicated database names")
+    
+    return all_names
+
+def verify_if_1_maindb(df):
+    driver_names = df[df['PURPOSE'] == 'main']['PATH']
+    n_names = driver_names.shape[0]
+    if n_names > 1:
+        raise ValueError("DB_LIST can only contain 1 main database")
+
+    return driver_names[0]
+
 def parselist():
     print("Parsing database list file...")
     df = clean_db_list(readlist())
 
-    #check for duplicatednames
-    all_names = df["DB_NAME"]
-    if all_names.duplicated().any():
-        raise ValueError("DB_LIST has duplicated database names")
+    #check for duplicated names
+    all_names = verify_if_any_duplicates(df)
 
     #get the driver name
-    driver_name = df[df['PURPOSE'] == 'main']['PATH'][0]
+    driver_name = verify_if_1_maindb(df)
 
     primary_dbs = df[df['PURPOSE'] == 'primary'] # Filter for primary databases
 
