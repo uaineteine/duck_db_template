ef4e7dd4fc70dff38f535b13b97449e9ad4a6a03
Daniel <39141263+uaineteine@users.noreply.github.com>
2025-02-01 19:20:24 +1100

    Update start_db.py

--- start_db.py
+++ start_db.py
@@ -1,15 +1,16 @@
 import duckdb
 import pandas as pd
 
+# Read the CSV file using pandas
+df = pd.read_csv(csv_file, delimiter='|')
+
+driver_name = df[df['PURPOSE'].str.strip() == 'main']['PATH'][0]
+
 # Connect to the driver database
-driver_db = 'haupt.db'
-con = duckdb.connect(driver_db)
+con = duckdb.connect(haupt_name)
 
 # Path to the CSV file
 csv_file = 'db_list.csv'
-
-# Read the CSV file using pandas
-df = pd.read_csv(csv_file, delimiter='|')
 
 # Attach primary databases
 primary_dbs = df[df['PURPOSE'].str.strip() == 'primary'] # Filter for primary databases
@@ -17,4 +18,4 @@ primary_dbs = df[df['PURPOSE'].str.strip() == 'primary'] # Filter for primary da
 for index, row in primary_dbs.iterrows():
     con.execute(f"ATTACH DATABASE '{row['PATH'].strip()}' AS {row['DB_NAME'].strip()}")
 
-print("Primary databases attached successfully."
+print("Primary databases attached successfully.")
