4d7a50afefd9d8d76c67f6506a83dace91daaac9
Daniel Stamer-Squair <uaine.teine@hotmail.com>
2025-02-01 21:18:45 +1100

    added an error check

--- parse_db_list.py
+++ parse_db_list.py
@@ -16,10 +16,14 @@ def parselist():
     print("Parsing database list file...")
     df = clean_db_list(readlist())
 
+    #check for duplicatednames
+    all_names = df["DB_NAME"]
+    if all_names.duplicated().any():
+        raise ValueError("DB_LIST has duplicated database names")
+
     #get the driver name
     driver_name = df[df['PURPOSE'] == 'main']['PATH'][0]
 
-    #check the primary_names
     primary_dbs = df[df['PURPOSE'] == 'primary'] # Filter for primary databases
 
-    return driver_name, df["DB_NAME"], primary_dbs
+    return driver_name, all_names, primary_dbs
