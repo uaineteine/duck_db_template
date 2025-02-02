c3fcf68e9a6384aa7f64f5f9cb411413fe560cb1
Daniel <39141263+uaineteine@users.noreply.github.com>
2025-02-02 14:24:33 +1100

    Update start_db.py

--- start_db.py
+++ start_db.py
@@ -33,7 +33,7 @@ con = start_db()
 
 con.sql("CREATE TABLE users.test (i INTEGER)")
 con.sql("CREATE TABLE models.test (k INTEGER)")
-con.sql("CREATE TABLE annex1.test (f INTEGER)")
+con.sql("CREATE TABLE a1.test (f INTEGER)")
 
 #return the table list
 metadata.get_inventory(con).show()
