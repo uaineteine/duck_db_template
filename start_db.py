4a767e6649d0f605bf5175081e32bf0a17ebaa13
Daniel <39141263+uaineteine@users.noreply.github.com>
2025-02-05 17:29:01 +1100

    keep execution simplified

--- start_db.py
+++ start_db.py
@@ -31,9 +31,8 @@ def start_db():
 
 con = start_db()
 
-con.sql("CREATE TABLE users.test (i INTEGER)")
-con.sql("CREATE TABLE models.test (k INTEGER)")
-con.sql("CREATE TABLE a1.test (f INTEGER)")
-
+#con.sql("CREATE TABLE users.test (i INTEGER)")
+#con.sql("CREATE TABLE models.test (k INTEGER)")
+#con.sql("CREATE TABLE a1.test (f INTEGER)")
 #return the table list
-metadata.get_inventory(con).show()
+#metadata.get_inventory(con).show()
