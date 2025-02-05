b10d660ceda76002c86c5c9315c1ef74dbcff57b
Daniel <39141263+uaineteine@users.noreply.github.com>
2025-02-05 17:37:10 +1100

    Create test_start_db.py

---
+++ test_start_db.py
@@ -0,0 +1,13 @@
+import metadata
+import start_db as sdb
+
+con = sdb.start_db()
+
+con.sql("CREATE TABLE users.test (i INTEGER)")
+con.sql("CREATE TABLE models.test (k INTEGER)")
+con.sql("CREATE TABLE a1.test (f INTEGER)")
+
+return the table list
+metadata.get_inventory(con).show()
+
+con.close()
