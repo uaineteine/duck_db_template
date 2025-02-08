4af8958cb3167d3bb8c6288a3be7d2a800e4aa3a
Daniel Stamer-Squair <uaine.teine@hotmail.com>
2025-02-08 17:30:08 +1100

    init table script works for new instances

--- test_start_db.py
+++ test_start_db.py
@@ -4,14 +4,14 @@ import start_db as sdb
 
 con = sdb.start_db()
 
-con.sql("CREATE TABLE IF NOT EXISTS users.test (i INTEGER)")
-con.sql("CREATE TABLE IF NOT EXISTS models.test (k INTEGER)")
-con.sql("CREATE TABLE IF NOT EXISTS a1.test (f INTEGER)")
-
-#return the table list
-metadata.get_inventory(con).show()
+#con.sql("CREATE TABLE IF NOT EXISTS users.test (i INTEGER)")
+#con.sql("CREATE TABLE IF NOT EXISTS models.test (k INTEGER)")
+#con.sql("CREATE TABLE IF NOT EXISTS a1.test (f INTEGER)")
 
 #attempt to make new tables
 init_tables_from_list.init_these_tables(con, "init_tables/new_tables.csv")
+
+#return the table list
+metadata.get_inventory(con).show()
 
 con.close()
