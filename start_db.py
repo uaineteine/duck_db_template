249c316418215d7d370afe109b324fd90b52ddc7
Daniel Stamer-Squair <uaine.teine@hotmail.com>
2025-02-08 14:12:06 +1100

    generalised attach db method

--- start_db.py
+++ start_db.py
@@ -1,8 +1,14 @@
 import duckdb
-import os
 from modules import metadata
 from modules import parse_db_list
 from modules import fileio
+
+def attach_db(con, path, name, readonly=False):
+    fileio.create_filepath_dirs(path)
+    ex_string = f"ATTACH DATABASE '{path}' AS {name}"
+    if (readonly):
+        ex_string += " (READ_ONLY)"
+    con.execute(ex_string)
 
 def create_and_attach_dbs():
     # Read the CSV file using pandas
@@ -14,13 +20,11 @@ def create_and_attach_dbs():
 
     # Attach primary databases
     for i, row in primary_dbs.iterrows():
-        fileio.create_filepath_dirs(row['PATH'])
-        con.execute(f"ATTACH DATABASE '{row['PATH']}' AS {row['DB_NAME']}")
+        attach_db(con, row['PATH'], row['DB_NAME'])
 
     # Attach secondary databases as read only
     for i, row in secondary_dbs.iterrows():
-        fileio.create_filepath_dirs(row['PATH'])
-        con.execute(f"ATTACH DATABASE '{row['PATH']}' AS {row['DB_NAME']} (READ_ONLY)")
+        attach_db(con, row['PATH'], row['DB_NAME'], readonly=True)
 
     attached = metadata.get_attached_dbs(con)
     print("Attached the following databases")
