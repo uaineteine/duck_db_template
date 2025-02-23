c52bd87e22f4f78fac311706b6786863347c0cdc
Daniel <39141263+uaineteine@users.noreply.github.com>
2025-02-23 23:12:27 +1100

    version_bump

--- launcher/start_db.py
+++ launcher/start_db.py
@@ -8,7 +8,7 @@ from modules import dbmet
 from modules import parse_db_list
 from modules import init_tables_from_list
 
-DB_VER = "1.2.5"
+DB_VER = "1.2.6"
 print(DB_VER)
 
 def attach_db(con, path, name, readonly=False):
