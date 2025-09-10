57c61c8a6dcecf18a34b00115ced35030f0622bc
Daniel Stamer-Squair <daniel.robert.s-s@hotmail.com>
2025-09-10 21:37:46 +1000

    create views module

---
+++ launcher/views.py
@@ -0,0 +1,26 @@
+import pandas as pd
+
+def get_db_views(con) -> pd.DataFrame:
+    """
+    Retrieve all views from the connected DuckDB database.
+
+    Parameters:
+    ----------
+    con : duckdb.DuckDBPyConnection
+        An active DuckDB connection object.
+
+    Returns:
+    -------
+    pandas.DataFrame
+        A DataFrame containing the names and SQL definitions of all views
+        in the current DuckDB session.
+    """
+    sql_query = "SELECT view_name AS VIEW_NAME, sql AS SQL FROM duckdb_views();"
+    df = con.sql(sql_query).df()
+
+    return df
+
+def read_db_csv(tbl_views_path:str) -> pd.DataFrame:
+    df = pd.read_csv(tbl_views_path)
+
+    return df
