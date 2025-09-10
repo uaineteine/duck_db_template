e36789f0b6556c0a3f6e9c94cf2fbb4e366a570d
Daniel Stamer-Squair <daniel.robert.s-s@hotmail.com>
2025-09-10 22:08:09 +1000

    views creation code made own method

--- launcher/start_db.py
+++ launcher/start_db.py
@@ -137,23 +137,6 @@ def start_db(def_tables_path="init_tables"):
     if not salt_checking(con):
         raise ValueError("SALT_CHECK value mismatch. Potential integrity issue detected.")
     
-    #get the views
-    db_views = views.get_db_views(con)
-    csv_views = views.read_db_csv(os.path.join(def_tables_path, "views.csv"))
-    remaining_views = pd.concat([csv_views, db_views, db_views]).drop_duplicates(keep=False)
-
-    # Assuming remaining_views is a DataFrame with columns: VIEW_NAME and SQL
-    for _, row in remaining_views.iterrows():
-        view_name = row['VIEW_NAME']
-        sql_query = row['SQL']
-        
-        # Construct the CREATE VIEW statement
-        create_view_stmt = f"CREATE VIEW {view_name} AS {sql_query};"
-        
-        # Execute the statement in DuckDB
-        con.execute(create_view_stmt)
-
-    d = con.execute("SELECT * FROM db_num_tables_view;").df()
-    print(d)
+    views.setupviews(con, def_tables_path)
 
     return con
--- launcher/views.py
+++ launcher/views.py
@@ -1,3 +1,4 @@
+import os
 import pandas as pd
 from uainepydat import dataio
 
@@ -25,3 +26,23 @@ def read_db_csv(tbl_views_path:str) -> pd.DataFrame:
     df = dataio.read_flat_psv(tbl_views_path)
 
     return df
+
+def setupviews(con, def_tables_path):
+    #get the views
+    db_views = get_db_views(con)
+    csv_views = read_db_csv(os.path.join(def_tables_path, "views.csv"))
+    remaining_views = pd.concat([csv_views, db_views, db_views]).drop_duplicates(keep=False)
+
+    # Assuming remaining_views is a DataFrame with columns: VIEW_NAME and SQL
+    for _, row in remaining_views.iterrows():
+        view_name = row['VIEW_NAME']
+        sql_query = row['SQL']
+        
+        # Construct the CREATE VIEW statement
+        create_view_stmt = f"CREATE VIEW {view_name} AS {sql_query};"
+        
+        # Execute the statement in DuckDB
+        con.execute(create_view_stmt)
+
+    #d = con.execute("SELECT * FROM db_num_tables_view;").df()
+    #print(d)
