ef07d167ad62786f3f49b529bfdd99b0648a3f03
Daniel Stamer-Squair <daniel.robert.s-s@hotmail.com>
2025-09-11 22:35:59 +1000

    db views now displayed on launch

--- launcher/views.py
+++ launcher/views.py
@@ -2,7 +2,7 @@ import os
 import pandas as pd
 from uainepydat import dataio
 
-def get_db_views(con) -> pd.DataFrame:
+def get_db_views(con):
     """
     Retrieve all views from the connected DuckDB database.
 
@@ -18,7 +18,7 @@ def get_db_views(con) -> pd.DataFrame:
         in the current DuckDB session.
     """
     sql_query = "SELECT view_name AS VIEW_NAME, sql AS SQL FROM duckdb_views();"
-    df = con.sql(sql_query).df()
+    df = con.sql(sql_query)
 
     return df
 
@@ -29,7 +29,7 @@ def read_db_csv(tbl_views_path:str) -> pd.DataFrame:
 
 def setupviews(con, def_tables_path):
     # Get the views
-    db_views = get_db_views(con)
+    db_views = get_db_views(con).df()
     csv_views = read_db_csv(os.path.join(def_tables_path, "views.csv"))
 
     # Only create views from csv_views that do not already exist in db_views
@@ -41,6 +41,11 @@ def setupviews(con, def_tables_path):
         sql_query = row['SQL']
         create_view_stmt = f"CREATE VIEW {view_name} AS {sql_query};"
         con.execute(create_view_stmt)
+        print(f"Created view: {view_name}\nSQL: {sql_query}\n")
+
+    # After all views are created, display the list of all views in the DB
+    print("\nAll views in the database:")
+    all_views = get_db_views(con).show()
 
     #d = con.execute("SELECT * FROM db_num_tables_view;").df()
     #print(d)
